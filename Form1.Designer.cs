c011ab5ae01e067ce527a807d5df0c9a95e24eee
metal0l <metal0l@outlook.com>
2020-06-03 12:17:16 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Minakova1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.А = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Б = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.в = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Г = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Д = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Е = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ж = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.З = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.И = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.К = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Л = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.М = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Н = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.О = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Длинна = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.А,
+            this.Б,
+            this.в,
+            this.Г,
+            this.Д,
+            this.Е,
+            this.Ж,
+            this.З,
+            this.И,
+            this.К,
+            this.Л,
+            this.М,
+            this.Н,
+            this.О});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(339, 342);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // А
+            // 
+            this.А.HeaderText = "А";
+            this.А.Name = "А";
+            this.А.Width = 20;
+            // 
+            // Б
+            // 
+            this.Б.HeaderText = "Б";
+            this.Б.Name = "Б";
+            this.Б.Width = 20;
+            // 
+            // в
+            // 
+            this.в.HeaderText = "В";
+            this.в.Name = "в";
+            this.в.Width = 20;
+            // 
+            // Г
+            // 
+            this.Г.HeaderText = "Г";
+            this.Г.Name = "Г";
+            this.Г.Width = 20;
+            // 
+            // Д
+            // 
+            this.Д.HeaderText = "Д";
+            this.Д.Name = "Д";
+            this.Д.Width = 20;
+            // 
+            // Е
+            // 
+            this.Е.HeaderText = "Е";
+            this.Е.Name = "Е";
+            this.Е.Width = 20;
+            // 
+            // Ж
+            // 
+            this.Ж.HeaderText = "Ж";
+            this.Ж.Name = "Ж";
+            this.Ж.Width = 20;
+            // 
+            // З
+            // 
+            this.З.HeaderText = "З";
+            this.З.Name = "З";
+            this.З.Width = 20;
+            // 
+            // И
+            // 
+            this.И.HeaderText = "И";
+            this.И.Name = "И";
+            this.И.Width = 20;
+            // 
+            // К
+            // 
+            this.К.HeaderText = "К";
+            this.К.Name = "К";
+            this.К.Width = 20;
+            // 
+            // Л
+            // 
+            this.Л.HeaderText = "Л";
+            this.Л.Name = "Л";
+            this.Л.Width = 20;
+            // 
+            // М
+            // 
+            this.М.HeaderText = "М";
+            this.М.Name = "М";
+            this.М.Width = 20;
+            // 
+            // Н
+            // 
+            this.Н.HeaderText = "Н";
+            this.Н.Name = "Н";
+            this.Н.Width = 20;
+            // 
+            // О
+            // 
+            this.О.HeaderText = "О";
+            this.О.Name = "О";
+            this.О.Width = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Размерность массива";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(561, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Критические работы";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(564, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 38);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Загрузить длинны";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 38);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Загрузить данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Длинна});
+            this.dataGridView2.Location = new System.Drawing.Point(344, 49);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(105, 342);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // Длинна
+            // 
+            this.Длинна.HeaderText = "Длинна";
+            this.Длинна.Name = "Длинна";
+            this.Длинна.Width = 50;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(455, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 38);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Вычислить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4,
+            this.Column1,
+            this.Column2,
+            this.Column5});
+            this.dataGridView3.Location = new System.Drawing.Point(455, 49);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(323, 367);
+            this.dataGridView3.TabIndex = 11;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Ранний старт";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 50;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Ранний финиш";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 50;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Поздний старт";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 53;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Поздний финиш";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 53;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Резерв";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 50;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "14";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 419);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Вальков К.И. Вариант 4";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn А;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Б;
+        private System.Windows.Forms.DataGridViewTextBoxColumn в;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Г;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Д;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Е;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ж;
+        private System.Windows.Forms.DataGridViewTextBoxColumn З;
+        private System.Windows.Forms.DataGridViewTextBoxColumn И;
+        private System.Windows.Forms.DataGridViewTextBoxColumn К;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Л;
+        private System.Windows.Forms.DataGridViewTextBoxColumn М;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Н;
+        private System.Windows.Forms.DataGridViewTextBoxColumn О;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Длинна;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Minakova1
+{
+    public partial class Form1 : Form
+    {
+       
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] row = new string[18];
+        int M = 0;
+        int[] MassVremeni = new int[14];
+        int[,] MainMass = new int[0, 0];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            row[0] = "А";
+            row[1] = "Б";
+            row[2] = "В";
+            row[3] = "Г";
+            row[4] = "Д";
+            row[5] = "Е";
+            row[6] = "Ж";
+            row[7] = "З";
+            row[8] = "И";
+            row[9] = "К";
+            row[10] = "Л";
+            row[11] = "М";
+            row[12] = "Н";
+            row[13] = "О";
+            row[14] = "П";
+            row[15] = "Р";
+            row[16] = "С";
+            row[17] = "Т";
+            string[] str1;                        
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {        
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";                
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {                                                          
+                    var fileStream = openFileDialog.OpenFile();
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                    {
+                        str1 = reader.ReadToEnd().Split(new Char[] { ' ', '\r' });
+                        dataGridView2.ColumnCount = 1;
+                        dataGridView2.RowCount = M;
+                        int pos1 = 0;
+                               for (int j = 0; j < dataGridView2.RowCount; j++)
+                               {                     
+                                   dataGridView2[0, j].Value = str1[pos1];
+                                   MassVremeni[j] = Convert.ToInt32(str1[pos1]);
+                                  pos1++;
+                               }
+                    }
+                }
+            }
+            for (int i = 0; i < M; i++)
+            {
+                dataGridView2.Rows[i].HeaderCell.Value = row[i];
+            }
+            dataGridView2.RowHeadersWidth = 50;
+        }
+        // ресайз массива
+        public static void ResizeArray<T>(ref T[,] original, int cols, int rows)
+        {
+            T[,] newArray = new T[rows, cols];
+            Array.Copy(original, newArray, original.Length);
+            original = newArray;
+        }
+        //  работы предшествия
+        private void button2_Click(object sender, EventArgs e)
+        {          
+            string[] str;           
+            dataGridView1.RowHeadersWidth = 50;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                    {
+                        str = reader.ReadToEnd().Split(new Char[] { ' ', '\r' });
+                       // double N = Math.Sqrt(str.Length);
+                        M = Convert.ToInt32(textBox3.Text);
+                        int pos = 0;
+                        dataGridView1.ColumnCount = M;
+                        dataGridView1.RowCount = M;
+                        ResizeArray(ref MainMass, M, M);
+                        for (int i = 0; i < dataGridView1.RowCount; i++)
+                        {
+                            for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                            {
+                                if (str[pos] != "")
+                                    dataGridView1[j, i].Value = str[pos];
+                                    MainMass[i, j] = Convert.ToInt32(str[pos]);
+                                pos++;
+                            }
+                            dataGridView1.Rows[i].HeaderCell.Value = row[i];
+                        }
+                    }
+                }
+            }            
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView3.RowCount = M;
+            dataGridView3.ColumnCount = 5;
+            int[] MassNachalnyxRabot = new int[3];
+            int[] MassEarlyStart = new int[M];
+            int k = 0;
+            int[] MassEarlyFinish = new int[M];
+
+            // поиск начальных работ           
+            for (int i = 0; i < M; i++)
+            {
+                int summ = 0;
+                
+                for (int j = 0; j < M; j++)
+                {
+
+                    summ = summ + MainMass[i, j];
+                }
+                if (summ < 1)
+                {
+                    MassNachalnyxRabot[k] = i;
+                    k = k + 1;
+                }
+            }
+            // подсчет ранних финишей и старотв начальных работ
+            for (int i = 0; i < MassNachalnyxRabot.Length ; i++)
+            {
+                MassEarlyStart[MassNachalnyxRabot[i]] = 1;
+                MassEarlyFinish[MassNachalnyxRabot[i]] = MassEarlyStart[MassNachalnyxRabot[i]] + MassVremeni[MassNachalnyxRabot[i]] - 1;
+            }
+            
+            // заполнение ранних стартов и финишей остальных работ
+            int[] massprom = new int[4];
+            int max;
+            int u = 0;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j =0; j < M; j++)
+                {
+                    if (MainMass[i,j] > 0)
+                    {
+                        massprom[u] = MassEarlyFinish[j];
+                        u = u + 1;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                if (massprom[0] > 0)
+                {
+                    max = massprom.Max();
+                    MassEarlyStart[i] = max +1;
+                    MassEarlyFinish[i] = MassEarlyStart[i] + MassVremeni[i] - 1;
+                    Array.Clear(massprom, 0, massprom.Length);
+                    u = 0;
+                    max = 0;
+                }
+            }
+            // поиск финальных работ
+            int[] MassFinRabot = new int[3];
+            int[] MassLateStart = new int[M];
+            int t = 0;
+            int[] MassLateFinish = new int[M];                 
+            for (int j = 0; j < M; j++)
+            {
+                int summ = 0;
+
+                for (int i = 0; i < M; i++)
+                {
+                    summ = summ + MainMass[i, j];
+                }
+                if (summ < 1)
+                {
+                    MassFinRabot[t] = j;
+                    t = t + 1;
+                }
+            }
+            // подсчет поздних финишей и старотв финальных работ
+            int[] massprom1 = new int[4];
+            for (int i =0; i <4;i++)
+            {
+                massprom1[i] = 1000;
+            }
+            int min;
+            int u1 = 0;
+            for (int i =0; i< MassFinRabot.Length; i++)
+            {
+                MassLateFinish[MassFinRabot[i]] = MassEarlyFinish[MassFinRabot[i]];
+                
+            }
+            int maxLateFinish = MassLateFinish.Max();
+            for ( int i = 0; i< MassLateFinish.Length; i++)
+            {
+                if (MassLateFinish[i] > 0)
+                {
+                    MassLateFinish[i] = maxLateFinish;
+                }
+            }
+            for (int i = 0; i < MassFinRabot.Length; i++)
+            {
+                MassLateStart[MassFinRabot[i]] = MassLateFinish[MassFinRabot[i]] - MassVremeni[MassFinRabot[i]] + 1;
+            }
+            // заполнение поздних стартов и финишей остальных работ
+            for (int j = M-1; j > -1; j--)
+            {
+                for (int i = M-1; i> -1; i--)
+                {
+                    if (MainMass[i, j] > 0)
+                    {
+                        massprom1[u1] =  MassLateStart[i];
+                        u1 = u1 + 1;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                if (massprom1[0] < 500)
+                {
+                    min = massprom1.Min();
+                    MassLateFinish[j] = min - 1;
+                    MassLateStart[j] = MassLateFinish[j] - MassVremeni[j] + 1;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        massprom1[i] = 1000;
+                    }
+                    u1 = 0;
+                    min = 0;
+                }
+            }
+            // резервы
+            int[] MassRezerv = new int[M];
+            for (int i =0; i < M; i++)
+            {
+                MassRezerv[i] = MassLateFinish[i] - MassEarlyFinish[i];
+                dataGridView3.Rows[i].Cells[4].Value = MassRezerv[i];
+            }
+            // вывод всех массивов в гриды 2 и 3      
+            for (int j = 0; j < M; j++)
+            {
+                dataGridView3.Rows[j].Cells[2].Value = MassLateStart[j];
+            }
+            for (int j = 0; j < M; j++)
+            {
+                dataGridView3.Rows[j].Cells[3].Value = MassLateFinish[j];
+            }           
+            for (int j = 0; j < M; j++)
+            {
+                dataGridView3.Rows[j].Cells[0].Value = MassEarlyStart[j];               
+            }
+            for (int j = 0; j < M; j++)
+            {                
+                  dataGridView3.Rows[j].Cells[1].Value = MassEarlyFinish[j];
+            }
+            // заполнение букв
+            for (int i = 0; i < M; i++)
+            {
+                dataGridView3.Rows[i].HeaderCell.Value = row[i];
+            }
+            dataGridView3.RowHeadersWidth = 50;
+
+            // критические работы
+            for (int i = 0; i < M; i++)
+            {
+                if (MassRezerv[i] == 0)
+                {
+                    textBox1.Text = textBox1.Text + " ->" + Convert.ToString(row[i]);
+                }
+                
+            }
+            
+        }
+    }
+}
